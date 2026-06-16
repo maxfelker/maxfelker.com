a460558e008d8ebba8a956e8130641a6fc577450
Max Felker <official@maxfelker.com>
2026-06-15 17:36:37 -0700

    Updated about me section

--- src/HomePage/index.jsx
+++ src/HomePage/index.jsx
@@ -7,10 +7,10 @@ export default function HomePage() {
   return (
     <div className={styles.content}>
       <h1>Hi, I'm Max</h1>
-      <p>My name is Max and I make things happen. My super power is accelerating creative and enterprise product teams. I am passionate about fostering safe spaces where people thrive. </p>
-
-      <p>Today, I work at Microsoft where I focus on cross-functional technology and strategy innovation at scale. With over 17 years of experience, I am a trusted and versatile leader.</p>
-
+      <p>My name is Max and my super power is realizing the art-of-the-possible. </p>
+      
+      <p>With two decades of experience in technology across disciplines and industries, I am a trusted leader who knows how to take ideas from 0 to 1 and products from 1 to 10. </p>
+      
       <p>Learn more <a href="/about">about me</a>.</p>
 
       {articles.length > 0 && (
